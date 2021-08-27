93b699f8a6a949511c023bf3a2a139c2b9a3b602
barzottoribeirolucas <barzottoribeirolucas@gmail.com>
2021-08-27 11:02:41 -0300

    Implementa um mock para o hr-payroll

---
+++ hr-payroll/src/main/java/com/lucas/ms/hrpayroll/services/PaymentService.java
@@ -0,0 +1,13 @@
+package com.lucas.ms.hrpayroll.services;
+
+import com.lucas.ms.hrpayroll.entities.Payment;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentService {
+
+    public Payment getPayment(long workerId, int days) {
+        return new Payment("Bob", 200.0, days);
+    }
+
+}
