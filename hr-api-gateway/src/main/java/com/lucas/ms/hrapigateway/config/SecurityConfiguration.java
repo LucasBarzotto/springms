c8aa17e8c316db353ec74fb6eb35c4ce303183a4
Lucas Ribeiro Barzotto <lucas.barzotto@db1.com.br>
2021-09-06 11:36:25 -0300

    Adicionando actuator

---
+++ hr-api-gateway/src/main/java/com/lucas/ms/hrapigateway/config/SecurityConfiguration.java
@@ -0,0 +1,18 @@
+package com.lucas.ms.hrapigateway.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.httpBasic().disable();
+        http.authorizeRequests().anyRequest().permitAll();
+    }
+}
